55780fbc34380560dad9e8289e421502b3ccc5cf
Guilherme Baptista <guisgb13@gmail.com>
2023-02-23 00:55:18 +0100

    Document the 'CallId' value class

--- app/src/main/java/com/xibasdev/sipcaller/dto/CallId.kt
+++ app/src/main/java/com/xibasdev/sipcaller/dto/CallId.kt
@@ -1,4 +1,7 @@
 package com.xibasdev.sipcaller.dto
 
+/**
+ * This value represents an unique identifier for a given call session or invitation.
+ */
 @JvmInline
 value class CallId internal constructor(val value: String)
